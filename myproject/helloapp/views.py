8fcc5a651601a12ffe306ab06216968f1b55a437
Loui <louis.geiger@stud.tu-darmstadt.de>
2024-12-08 15:43:06 +0100

    Oopsie, forgot to commit views.py

--- myproject/helloapp/views.py
+++ myproject/helloapp/views.py
@@ -1,10 +1,6 @@
 # Create your views here.
 
 # helloapp/views.py
-#from django.http import JsonResponse
-#from django.shortcuts import render
-
-# helloapp/views.py
 
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -12,7 +8,7 @@ from django.views.decorators.http import require_http_methods
 from .models import Entry
 import json
 from django.shortcuts import render, redirect
-from .forms import AccountCreateForm
+from .forms import AccountCreateForm, LoginForm
 from django.views import View
 
 #def index(request):
@@ -52,7 +48,24 @@ def create_account(request):
             return JsonResponse({"error": "Account already exists"}, status=400)
     return JsonResponse({"errors": form.errors}, status=400)
     
+def login_view(request):
+    template_name = 'helloapp/login.html'
     
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            surname = form.cleaned_data['surname']
+            user = Entry.objects.get(first_name=first_name, surname=surname)
+            
+            # Custom authentication logic
+            # You can replace this with Django User model in the future.
+            request.session['user_id'] = user.id  # Storing user in session
+            return redirect('index')  # Redirect to a dashboard or home page
+    else:
+        form = LoginForm()
+
+    return render(request, template_name, {'form': form})    
 
 # The following is old code from the helloWorld prototype. I leave it here for now, if someone needs to look something up
 
