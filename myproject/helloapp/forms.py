0baeb70003ce5c2b659a8dafe91a25c2ca49591d
Nicola Kleiner <nicola.kleiner@stud.tu-darmstadt.de>
2024-12-12 15:54:45 +0100

    changed forms.py to be compatible with AbstractUser

--- myproject/helloapp/forms.py
+++ myproject/helloapp/forms.py
@@ -1,11 +1,16 @@
 from django import forms
 from django.contrib.auth import authenticate
 from django.core.exceptions import ValidationError
-from .models import Entry
+from .models import BuddyMatchingUser
+from django.contrib.auth.forms import UserCreationForm
 
-class AccountCreateForm(forms.ModelForm):
+
+class BuddyMatchingUserCreationForm(UserCreationForm):
+    #password = forms.CharField(widget=forms.PasswordInput)
+    #password_confirmation = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    
     class Meta: 
-        model = Entry
+        model = BuddyMatchingUser
         fields = [
             'role', 'surname', 'first_name', 'preferred_language', 
             'email', 'degree_level', 'app_matr_number', 
@@ -23,10 +28,43 @@ class AccountCreateForm(forms.ModelForm):
             'country': forms.TextInput(attrs={'class': 'form-control'}),
             'preferred_number_of_partners': forms.NumberInput(attrs={'class': 'form-control', 'data-role-field': 'Buddy'}),
         }
-        
-        
+
+    def clean(self):
+        cleaned_data = super().clean()
+        """
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+
+        if password1 != password2:
+            raise forms.ValidationError("Passwords do not match")
+        """
+        return cleaned_data
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if BuddyMatchingUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("There is already an existing user with this email adress.")
+        return email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        """
+        user.set_password(self.cleaned_data['password1'])  # Passwort verschlüsseln
+        if commit:
+            user.save()
+
+    
+        """
+        user.username = self.cleaned_data['email']
+        if commit:
+            user.save()
+
+        return user
+
+
+
     def __init__(self, *args, **kwargs):
-        super(AccountCreateForm, self).__init__(*args, **kwargs)
+        super(BuddyMatchingUserCreationForm, self).__init__(*args, **kwargs)
 
         # Always hide 'partners' field
         if 'partners' in self.fields:
@@ -34,22 +72,25 @@ class AccountCreateForm(forms.ModelForm):
             
         if 'is_permitted' in self.fields:
             self.fields.pop('is_permitted')     
-            
+
+
+
+
+
+
 class LoginForm(forms.Form):
-    first_name = forms.CharField(max_length=255, label="First Name")
-    surname = forms.CharField(max_length=255, label="Surname")
+    email = forms.EmailField(label="Email", widget=forms.EmailInput)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
 
     def clean(self):
         cleaned_data = super().clean()
-        first_name = cleaned_data.get("first_name")
-        surname = cleaned_data.get("surname")
-
+        email = cleaned_data.get("email")
+        password = cleaned_data.get("password")
+        # Authentifizierung über das BuddyMatchingUser-Modell
         try:
-            user = Entry.objects.get(first_name=first_name, surname=surname)
-        except Entry.DoesNotExist:
-            raise ValidationError("Invalid Entry. Have you made a spelling error?")
-        
-        #if not user.is_permitted:
-        #    raise ValidationError("This account is not permitted to log in.")
-        
-        return cleaned_data            
+            user = BuddyMatchingUser.objects.get(email=email)
+            if not user.check_password(password):
+                raise forms.ValidationError("Invalid email or password")
+        except BuddyMatchingUser.DoesNotExist:
+            raise forms.ValidationError("Invalid email or password")
+        return cleaned_data         
