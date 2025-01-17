df8e624fde34ce4f5a27391249480b2751607afa
malinviveka <malin.schobert@stud.tu-darmstadt.de>
2025-01-17 14:48:26 +0100

    Added another filter in run_matching to match latecommers

--- myproject/helloapp/matching.py
+++ myproject/helloapp/matching.py
@@ -2,6 +2,7 @@ from helloapp.models import BuddyMatchingUser
 from .matching_utils import create_preference_lists
 from collections import defaultdict, deque
 from django.db import transaction
+from django.db.models import Count, F
 
 # student = international student
 
@@ -53,9 +54,9 @@ def gale_shapley(students, buddies, student_preferences, buddy_preferences):
 
 def run_matching():
     # load data
-    buddies = BuddyMatchingUser.objects.filter(role='Buddy', is_permitted=True)
-    students = BuddyMatchingUser.objects.filter(role='International Student', is_permitted=True)
-    
+    buddies = BuddyMatchingUser.objects.filter(role='Buddy', is_permitted=True).annotate(partner_count=Count('partners')).filter(preferred_number_of_partners__gte=F('partner_count'))
+    students = BuddyMatchingUser.objects.filter(role='International Student', is_permitted=True).annotate(partner_count=Count('partners')).filter(partner_count=0)
+  
     # create preference lists
     student_preferences, buddy_preferences = create_preference_lists(students, buddies)
     
