36338980252602c61d939050d6e731a818e94106
Loui <Geiger.Louis@web.de>
2025-01-18 20:14:13 +0100

    Add 2-language-support for homepage-message (+preparations for following commits)

--- myproject/helloapp/models.py
+++ myproject/helloapp/models.py
@@ -232,8 +232,9 @@ class BuddyMatchingUser(AbstractUser):
 
 
 class HomepageText(models.Model):
-    content = models.TextField(default="Willkommen auf der Seite!")  # Standardtext
-    last_updated = models.DateTimeField(auto_now=True)  # Automatisch aktualisieren bei jeder Änderung
+    content_de = models.TextField(_("Content in German"), default="Willkommen auf der Seite!")
+    content_en = models.TextField(_("Content in English"), default="Welcome to the page!")
+    last_updated = models.DateTimeField(auto_now=True)  # Wird bei jeder Änderung automatisch aktualisiert
 
     def __str__(self):
         return f"Homepage Text (aktualisiert am {self.last_updated})"
