493aeb42359b15475729dcbc366cd0c020d45810
malinviveka <malin.schobert@stud.tu-darmstadt.de>
2025-03-16 10:07:51 +0100

    Fixed formatting

--- myproject/static/feedback/feedback.ts
+++ myproject/static/feedback/feedback.ts
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
     q9Details.style.display = "none";
   }
 
-  //Setup event listeners for dynamic fields 
+  //Setup event listeners for dynamic fields
   toggleDetails("id_q5", "q5_details_box", "no");
   toggleDetails("id_q8", "q8_details_box", "yes");
   toggleDetails("id_q9", "q9_details_box", "no");
